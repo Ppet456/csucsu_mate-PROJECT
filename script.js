4df898267cb6383c0732491e50226264ce7bf8a2
Ppet456 <ps.gila456@gmail.com>
2024-12-04 09:48:12 +0100

    js_felidoandwin

--- script.js
+++ script.js
@@ -5,6 +5,22 @@ let gol2d = document.getElementById("gol2");
 let time = 0;
 let idout = document.getElementById("ido_output");
 let ido_gomb = document.getElementById("start");
+let felido_output = document.getElementById("felido_output")
+let felido
+
+
+function felido_submit()
+{
+    felido = parseInt((document.getElementById("felido_input").value)) * 60 + parseInt(document.getElementById("felido_input_second").value);
+}
+
+
+
+
+
+
+
+let felclock
 function gol(x,gol)
 {
     switch(gol)
@@ -36,7 +52,38 @@ function clock()
 {
     time++;
     idout.innerHTML = `${Math.floor(time / 60)}:${time % 60}`;
+    if(time >= 1200)
+        {
+            clearInterval(ora)
+            if(felido_input.value != 0)
+                {
+                    felclock = setInterval(felido_clock,1000)//idö
+                }
+            else
+            {
+                win()
+            }
+
+        }
 }
+
+
+
+
+
+
+
+function felido_clock()
+{
+    felido--;
+    felido_output.innerHTML = `${Math.floor(felido / 60)}:${felido % 60}`;
+    if(felido == 0)
+        {
+            win()
+        }
+}
+
+
 
 let ora;
 let istimegoing = false;
@@ -52,14 +99,17 @@ function start()
         }
         else
         {
-            ora = setInterval(clock,1000)
+            ora = setInterval(clock,1)//idö
             istimegoing = true;
             ido_gomb.innerHTML = "stop";
         }
 }
 
 
-
+function win()
+{
+    log.console("win has been runed");
+}
 
 
 
